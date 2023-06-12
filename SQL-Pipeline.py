c568b19b0ecf75c9dcbe18e3d259688518288318
rrustad <rrustad>
2023-06-12 22:25:17 +0000

    update script

---
+++ SQL-Pipeline.py
@@ -0,0 +1,29 @@
+# Databricks notebook source
+# MAGIC %md-sandbox
+# MAGIC # Simplify ETL with Delta Live Table
+# MAGIC
+# MAGIC DLT makes Data Engineering accessible for all. Just declare your transformations in SQL or Python, and DLT will handle the Data Engineering complexity for you.
+# MAGIC
+# MAGIC <img style="float:right" src="https://github.com/QuentinAmbard/databricks-demo/raw/main/product_demos/dlt-golden-demo-loan-1.png" width="700"/>
+# MAGIC
+# MAGIC **Accelerate ETL development** <br/>
+# MAGIC Enable analysts and data engineers to innovate rapidly with simple pipeline development and maintenance 
+# MAGIC
+# MAGIC **Remove operational complexity** <br/>
+# MAGIC By automating complex administrative tasks and gaining broader visibility into pipeline operations
+# MAGIC
+# MAGIC **Trust your data** <br/>
+# MAGIC With built-in quality controls and quality monitoring to ensure accurate and useful BI, Data Science, and ML 
+# MAGIC
+# MAGIC **Simplify batch and streaming** <br/>
+# MAGIC With self-optimization and auto-scaling data pipelines for batch or streaming processing 
+# MAGIC
+# MAGIC ## Our Delta Live Table pipeline
+# MAGIC
+# MAGIC We'll be using as input a raw dataset containing information on our customers Loan and historical transactions. 
+# MAGIC
+# MAGIC Our goal is to ingest this data in near real time and build table for our Analyst team while ensuring data quality.
+
+# COMMAND ----------
+
+# Your code here
